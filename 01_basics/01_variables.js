7d1a6d57a3e88dd19892e89fd643c99683a95c6b
RahulGurjar07 <rahulgurjarmp09@gmail.com>
2024-03-23 22:51:39 +0530

    javasrcipt starting

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 343
+let name = 'Rahul gurjar'
+var age = 23
+city = 'Indore'
+let accountStatus;
+let accountType = null
+
+console.table({ accountId, name, age, city, accountStatus, accountType})
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Rahul")
+
+
+let name = "rahul"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => range of number is -2^53 to 2^53 something
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
